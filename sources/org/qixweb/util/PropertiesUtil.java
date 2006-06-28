5ec4039286ab7ed515776e7211acd4b711c3e3ad
xplayers <xplayers@a9f70300-554b-0410-95b5-b18b23f37aa1>
2006-06-28 13:31:04 +0000

    minor refactoring of PropertiesUtil

--- sources/org/qixweb/util/PropertiesUtil.java
+++ sources/org/qixweb/util/PropertiesUtil.java
@@ -13,18 +13,15 @@ public class PropertiesUtil
 
     public static Properties with(String key1, String value1, String key2, String value2)
     {
-        Properties properties = new Properties();
-        properties.setProperty(key1, value1);
-        properties.setProperty(key2, value2);
-        return properties;
+        Properties base = with(key1, value1);
+        base.setProperty(key2, value2);
+        return base;
     }
 
     public static Properties with(String key1, String value1, String key2, String value2, String key3, String value3)
     {
-        Properties properties = new Properties();
-        properties.setProperty(key1, value1);
-        properties.setProperty(key2, value2);
-        properties.setProperty(key3, value3);
-        return properties;
+        Properties base = with(key1, value1, key2, value2);
+        base.setProperty(key3, value3);
+        return base;
     }
 }

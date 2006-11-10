326ec496906613eb0b80a24bf9727eb2ca3ec7f4
xplayers <xplayers@a9f70300-554b-0410-95b5-b18b23f37aa1>
2006-11-10 16:34:59 +0000

    add method that returns the index of the nth occurence of a char in a string

--- sources/org/qixweb/util/StringUtil.java
+++ sources/org/qixweb/util/StringUtil.java
@@ -7,54 +7,73 @@ import java.util.regex.Pattern;
 import org.apache.commons.lang.StringUtils;
 
 public class StringUtil
-{	
+{
     public static final String EMPTY = "";
-    
-	public static boolean string_containsRegex(String text, String regex)
-	{
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(text);
-		return matcher.find();
-	}
 
-	public static String replace_with_in(String aStringToReplace, String aNewString, String aString)
-	{
-		Pattern pattern = Pattern.compile(aStringToReplace);
-		Matcher matcher = pattern.matcher(aString);
-		return matcher.replaceAll(aNewString);
-	}
+    public static int nthIndexOf(String aString, int occurrenceNumber, char aCharToFind)
+    {
+        int counter = 1;
 
-	public static String join(String[] strings, String separator)
-	{
-		if( strings.length == 0 ) {
-			return "";
-		}
+        char[] str = aString.toCharArray();
+        for (int i = 0; i < str.length; i++)
+        {
+            if (str[i] == aCharToFind)
+            {
+                if (counter == occurrenceNumber)
+                    return i;
+                counter++;
+            }
+        }
 
-		StringBuffer list = new StringBuffer(strings[0]);
-		for (int i = 1; i < strings.length; i++)
-			list.append(separator + strings[i]);			
-		
-		return list.toString();
-	}
-    
+        return -1;
+    }
+
+    public static boolean string_containsRegex(String text, String regex)
+    {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        return matcher.find();
+    }
+
+    public static String replace_with_in(String aStringToReplace, String aNewString, String aString)
+    {
+        Pattern pattern = Pattern.compile(aStringToReplace);
+        Matcher matcher = pattern.matcher(aString);
+        return matcher.replaceAll(aNewString);
+    }
+
+    public static String join(String[] strings, String separator)
+    {
+        if (strings.length == 0)
+        {
+            return "";
+        }
+
+        StringBuffer list = new StringBuffer(strings[0]);
+        for (int i = 1; i < strings.length; i++)
+            list.append(separator + strings[i]);
+
+        return list.toString();
+    }
+
     public static String join(Collection strings, String separator)
     {
-        return join((String[])strings.toArray(new String[0]), separator);
-    }    
+        return join((String[]) strings.toArray(new String[0]), separator);
+    }
 
-	public static String compactWhitespaces(String aString)
-	{
-		return StringUtils.join(StringUtils.split(aString), " ");
-	}
+    public static String compactWhitespaces(String aString)
+    {
+        return StringUtils.join(StringUtils.split(aString), " ");
+    }
 
     public static String extractFirst(String text, String regex)
     {
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(text);
-		if (matcher.find())
-		    return matcher.group();
-		else
-		    return null;
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        if (matcher.find())
+            return matcher.group();
+        else
+            return null;
     }
 
     public static boolean containsIgnoringCase(String container, String contained)
--- sources/org/qixweb/util/test/TestStringUtil.java
+++ sources/org/qixweb/util/test/TestStringUtil.java
@@ -1,4 +1,5 @@
 package org.qixweb.util.test;
+
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -6,60 +7,70 @@ import org.qixweb.util.StringUtil;
 
 public class TestStringUtil extends ExtendedTestCase
 {
-	public void testStringContainsIgnoringCase()
-	{
-		assertTrue("'abcdefg' should contain 'ab'", StringUtil.containsIgnoringCase("abcdefg", "ab"));
+    public void testNthIndexOf() throws Exception
+    {
+        assertEquals(-1, StringUtil.nthIndexOf("ciccionissimo", 1, 'u'));
+        
+        assertEquals(1, StringUtil.nthIndexOf("ciccionissimo", 1, 'i'));
+        assertEquals(4, StringUtil.nthIndexOf("ciccionissimo", 2, 'i'));
+        assertEquals(7, StringUtil.nthIndexOf("ciccionissimo", 3, 'i'));
+        assertEquals(10, StringUtil.nthIndexOf("ciccionissimo", 4, 'i'));
+    }
+
+    public void testStringContainsIgnoringCase()
+    {
+        assertTrue("'abcdefg' should contain 'ab'", StringUtil.containsIgnoringCase("abcdefg", "ab"));
         assertTrue("'abcdefg' should contain 'aB'", StringUtil.containsIgnoringCase("abcdefg", "aB"));
         assertTrue("'abcdefg' should contain 'AB'", StringUtil.containsIgnoringCase("abcdefg", "AB"));
-		assertFalse("'abcdefg' should *not* contain 'z'", StringUtil.containsIgnoringCase("abcdefg", "z"));
-	}
-	
-	public void testStringExtractFirst()
-	{
-		String aString = "abc12345def";
-		assertNull(StringUtil.extractFirst(aString, "\\d{10}"));
-		assertEquals("12345", StringUtil.extractFirst(aString, "\\d{5}"));
-	}	
-	
-	public void testStringContainsRegex()
-	{
-		String aString = "abcwob";
-		String containedRegex = "c.*b";
-		String notContainedRegex = "b.+c";
+        assertFalse("'abcdefg' should *not* contain 'z'", StringUtil.containsIgnoringCase("abcdefg", "z"));
+    }
 
-		assertTrue("'" +aString+ "'" + "should contain" + "'" +containedRegex+ "'", StringUtil.string_containsRegex(aString, containedRegex));
-		assertFalse("'" +aString+ "'" + "should not contain" + "'" +notContainedRegex+ "'", StringUtil.string_containsRegex(aString, notContainedRegex));
-	}
-	
-	public void testStringToReplaceNotFound()
-	{
-		String string = "Pippo Pluto";
-		assertEquals(string, StringUtil.replace_with_in("Paperino", "Pollo", string));
-	}
-	
-	public void testReduceMultipleWhitespacesToOne()
-	{
-		assertEquals("Pippo Pluto Paperino", StringUtil.compactWhitespaces("Pippo   Pluto  Paperino"));
-		assertEquals("Pippo Pluto Paperino", StringUtil.compactWhitespaces("           Pippo   Pluto Paperino"));
-		assertEquals("", StringUtil.compactWhitespaces(""));
-		assertEquals(null, StringUtil.compactWhitespaces(null));
-	}	
-	
-	public void testReplace()
-	{
-		String string = "Pippo Pluto Paperino";
-		assertEquals("Pippo Pluto Pollo", StringUtil.replace_with_in("Paperino", "Pollo", string));
-		
-		assertEquals("Gippo Gluto Gaperino", StringUtil.replace_with_in("P", "G", string));		
-		
-	}	
+    public void testStringExtractFirst()
+    {
+        String aString = "abc12345def";
+        assertNull(StringUtil.extractFirst(aString, "\\d{10}"));
+        assertEquals("12345", StringUtil.extractFirst(aString, "\\d{5}"));
+    }
 
-	public void testJoin()
-	{
-		assertEquals("", StringUtil.join(new String[0], ":"));
-		assertEquals("anystring", StringUtil.join(new String[] {"anystring"}, ":"));
-		assertEquals("Pippo:Pluto:Paperino", StringUtil.join(new String[] {"Pippo", "Pluto", "Paperino"}, ":"));
-	}
+    public void testStringContainsRegex()
+    {
+        String aString = "abcwob";
+        String containedRegex = "c.*b";
+        String notContainedRegex = "b.+c";
+
+        assertTrue("'" + aString + "'" + "should contain" + "'" + containedRegex + "'", StringUtil.string_containsRegex(aString, containedRegex));
+        assertFalse("'" + aString + "'" + "should not contain" + "'" + notContainedRegex + "'", StringUtil.string_containsRegex(aString, notContainedRegex));
+    }
+
+    public void testStringToReplaceNotFound()
+    {
+        String string = "Pippo Pluto";
+        assertEquals(string, StringUtil.replace_with_in("Paperino", "Pollo", string));
+    }
+
+    public void testReduceMultipleWhitespacesToOne()
+    {
+        assertEquals("Pippo Pluto Paperino", StringUtil.compactWhitespaces("Pippo   Pluto  Paperino"));
+        assertEquals("Pippo Pluto Paperino", StringUtil.compactWhitespaces("           Pippo   Pluto Paperino"));
+        assertEquals("", StringUtil.compactWhitespaces(""));
+        assertEquals(null, StringUtil.compactWhitespaces(null));
+    }
+
+    public void testReplace()
+    {
+        String string = "Pippo Pluto Paperino";
+        assertEquals("Pippo Pluto Pollo", StringUtil.replace_with_in("Paperino", "Pollo", string));
+
+        assertEquals("Gippo Gluto Gaperino", StringUtil.replace_with_in("P", "G", string));
+
+    }
+
+    public void testJoin()
+    {
+        assertEquals("", StringUtil.join(new String[0], ":"));
+        assertEquals("anystring", StringUtil.join(new String[] { "anystring" }, ":"));
+        assertEquals("Pippo:Pluto:Paperino", StringUtil.join(new String[] { "Pippo", "Pluto", "Paperino" }, ":"));
+    }
 
     public void testJoinOnCollections()
     {
@@ -68,5 +79,5 @@ public class TestStringUtil extends ExtendedTestCase
         aCollectionOfNames.add("Pluto");
         aCollectionOfNames.add("Paperino");
         assertEquals("Pippo:Pluto:Paperino", StringUtil.join(aCollectionOfNames, ":"));
-    }   
+    }
 }
